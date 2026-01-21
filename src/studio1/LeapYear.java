ee978118ba8151339b4ed11344078184b1f6d6ca
fadumohussein113 <f.a.hussein@wustl.edu>
2026-01-21 13:56:25 -0600

    LeapYearCompleted

---
+++ src/studio1/LeapYear.java
@@ -0,0 +1,14 @@
+package studio1; 
+import java.util.Scanner;
+
+public class LeapYear {
+
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+        System.out.println("Enter the year: ");
+        int year = in.nextInt();
+        boolean isLeapYear = (year % 4 == 0) && ((year % 100 != 0) || (year % 400 == 0));
+        System.out.println("It is a leap year? " + isLeapYear);
+
+    }
+}
